2aa56a8de74c964d269377aa80ee98934ac44e74
Nayem12 <shanaeem773@gmail.com>
2022-12-10 11:34:58 +0600

    first commit

---
+++ src/components/AnimattedButton/AnimattedButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './AnimatedButton.css';
+
+const Button = (props) => {
+    return (
+        <button
+            className={`${props.outline ? 'button-outline' : 'button'
+                } bubbly-button ${props.className}`}
+            onClick={props.onClick}
+        >
+            <span className="button__text">{props.children}</span>
+        </button>
+    );
+};
+
+export default Button;
